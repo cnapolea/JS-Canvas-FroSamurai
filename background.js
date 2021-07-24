22a33c171dc189ca9ef7ba948d3757d40f43e025
cnapolea <cjnapoleao@gmail.com>
2021-07-24 01:53:33 +0100

    add: jail and princes to canvas

--- background.js
+++ background.js
@@ -9,6 +9,9 @@ class Background {
       `${this.imageUrl}bakcground_night2.png`,
       `${this.imageUrl}bakcground_night3.png`
     ];
+    this.prisonImg = `${this.imageUrl}prison1.png`;
+    this.princessImg = `${this.imageUrl}princess.png`;
+    this.frame = 0;
   }
 
   paint() {
@@ -23,5 +26,39 @@ class Background {
         this.game.display.height
       );
     }
+
+    const princess = new Image();
+    princess.src = this.princessImg;
+    this.game.ctx.save();
+
+    this.game.ctx.drawImage(
+      princess,
+      565 + 125 * Math.round(this.frame / 50),
+      15,
+      25,
+      25,
+      this.game.display.width - 100,
+      this.game.display.height - 68,
+      30,
+      50
+    );
+
+    this.game.ctx.restore();
+    this.frame++;
+    this.frame %= 70;
+
+    const prison = new Image();
+    prison.src = this.prisonImg;
+    this.game.ctx.drawImage(
+      prison,
+      100,
+      100,
+      151,
+      151,
+      this.game.display.width - 150,
+      this.game.display.height - 165,
+      151,
+      151
+    );
   }
 }
